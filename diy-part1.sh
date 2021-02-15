76d60c29cdbdaa6bed62f09ba03f59eba368dc81
lietxia <7046884+lietxia@users.noreply.github.com>
2021-02-15 11:51:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-mkdir package/base-files/files/config
+mkdir -p package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+export GOPROXY=https://goproxy.io,direct
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
