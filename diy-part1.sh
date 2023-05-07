c5e76dad500c26cf28eaf23df48ea2af808db1fc
lietxia <lietxia@gmail.com>
2023-05-07 16:10:37 +0800

    set

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,17 +14,9 @@
 mkdir -p package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-echo "src-git helloworld https://github.com/fw876/helloworld">>feeds.conf.default
+cp feeds.conf.default feeds.conf
+echo "src-git helloworld https://github.com/fw876/helloworld">>feeds.conf
 #sed -i 's/export GOPROXY/#export GOPROXY/g' package/lean/UnblockNeteaseMusicGo/Makefile;
 #sed -i 's/export GOPROXY/#export GOPROXY/g' package/lean/v2ray-plugin/Makefile;
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-
-#允许ROOT编译
-export FORCE_UNSAFE_CONFIGURE=1
-
-#报错修复
-
-cp /usr/bin/upx /workdir/openwrt/staging_dir/host/bin/
-cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
